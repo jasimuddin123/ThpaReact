6100756e748222755add951b31ad8742cbc3316c
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-10 17:46:18 -0700

    How to Render Multiple Elements inside ReactDOM.render() in ReactJS in Hindi #7

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 
 ReactDOM.render(
 
-<div>
-<h2>This is Jasim Uddin </h2>
-<h3>Working in Obeikan company</h3>
-</div>,
+[
+  <h2>This is Jaism Uddin </h2>,
+  <h3>Working in Obeikan Paper Industry</h3>,
+  <h4>Profession as Supervisor</h4>,
+],
   
   document.getElementById('root'))
