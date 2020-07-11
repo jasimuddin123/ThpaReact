8a7c65074e29b847d8406f61fdebf67188188647
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-11 06:33:27 -0700

    ES6 Template Literals in JSX in ReactJS in Hindi #11

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const myName = "Jasim Uddin";
+
+const myFirstName = "Jasim";
+const myLastName = "Uddin";
+
+
+
 ReactDOM.render(
   <>
-  <h2>My Name is {myName}</h2>
+  <h2>{`My name is ${myFirstName} ${myLastName}`}</h2>
+  <h2>{`My First Name is ${myFirstName} My last name is ${myLastName}`}</h2>
   <h2>This is Random Number {Math.random()}</h2>
   <h3>My Favorite Number is {2+2}</h3>
 
