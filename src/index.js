1fd9941b4f4e3987324789dfa110ae6eca82fc43
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-10 18:35:53 -0700

    Understanding React Fragment in React JS in Hindi in 2020 #8

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
+// ReactDOM.render(
 
-[
-  <h2>This is Jaism Uddin </h2>,
-  <h3>Working in Obeikan Paper Industry</h3>,
-  <h4>Profession as Supervisor</h4>,
-],
+// [
+//   <h2>This is Jaism Uddin </h2>,
+//   <h3>Working in Obeikan Paper Industry</h3>,
+//   <h4>Profession as Supervisor</h4>,
+// ],
+
+
   
-  document.getElementById('root'))
+  // document.getElementById('root'))
+
+  // ReactDOM.render(
+  //   <React.Fragment>
+  //     <h2>This is Jasim Uddin</h2>
+  //     <h3>As a Supervisor of Plant </h3>
+  //   </React.Fragment>,
+  //   document.getElementById('root')
+  // )
+
+  ReactDOM.render(
+    <>
+      <h2>This is Jasim Uddin </h2>
+      <h3>As a Plant Supervisor</h3>
+
+
+    </>,
+    document.getElementById('root')
+
+  )
