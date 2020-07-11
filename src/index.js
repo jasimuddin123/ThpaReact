f532c82f343e40e873eec913a3e6c69cb8639516
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-10 17:32:22 -0700

    JSX in React JS in Hindi in 2020 #6

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h2>Hello World!!!!</h2>, document.getElementById('root'))
+ReactDOM.render(<h2>JSX in React JS in Hindi in 2020 #6 </h2>, document.getElementById('root'))
