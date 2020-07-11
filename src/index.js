10fbecf45e56b9c1c4431a2259028efbc3454ebc
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-11 07:36:30 -0700

    JSX Attributes in ReactJS in Hindi | HTML Attribute Vs JSX Attribute in React | #13

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const currDate = new Date().toLocaleDateString();
-const currTime = new Date().toLocaleTimeString();
+
 const name = "Jasim Uddin";
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const img4 = "https://picsum.photos/350/300";
+const links = 'https://www.facebook.com/';
+
 ReactDOM.render(
 <>
-  <h2>{`My Name is ${name}`}</h2>
-  <h3>{`Today Current Date is = ${currDate}`}</h3>
-  <h3>{`Now Current Time is = ${currTime}`}</h3>
+  <h2 contentEditable="true">{`My Name is ${name}`}</h2>
+  <img src={img1} alt="Photo"/>
+  <img src={img2} alt="Photo"/>
+  <img src={img3} alt="Photo"/>
+  <img src={img4} alt="Photo"/>
+
+  <a href={links} target="_blanks">
+  <img src={img4} alt="Photo"/>
+  </a>
+
 </>,
   document.getElementById('root')
 )
