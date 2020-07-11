0b6ac807ba936fc5371fe19df657e3897d517e6c
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-10 18:48:35 -0700

    React JS Series Challenge #1: Create a Simple Web App on Top 5 Netflix Series List using JSX

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// ReactDOM.render(
+ReactDOM.render(
+  <>
+    <h2>My Favorite Person</h2>
+    <p>Who always help me </p>
 
-// [
-//   <h2>This is Jaism Uddin </h2>,
-//   <h3>Working in Obeikan Paper Industry</h3>,
-//   <h4>Profession as Supervisor</h4>,
-// ],
+    <ol>
+      <li>Zakir Naik</li>
+      <li>Mizanur Rahman</li>
+      <li>My Friend </li>
+      <li>My Brother</li>
+      <li>My Parents</li>
+    </ol>
 
+  </>,
+  document.getElementById('root')
 
-  
-  // document.getElementById('root'))
-
-  // ReactDOM.render(
-  //   <React.Fragment>
-  //     <h2>This is Jasim Uddin</h2>
-  //     <h3>As a Supervisor of Plant </h3>
-  //   </React.Fragment>,
-  //   document.getElementById('root')
-  // )
-
-  ReactDOM.render(
-    <>
-      <h2>This is Jasim Uddin </h2>
-      <h3>As a Plant Supervisor</h3>
-
-
-    </>,
-    document.getElementById('root')
-
-  )
+)
