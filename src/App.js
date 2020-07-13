317e3439ae4faf96b12005c943cdeb72e854b192
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-13 06:31:55 -0700

    React JS Practice #4- Rewrite our React Project into Components in React JS  #19.mp4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import Heading from './Heading';
-import Paragraph from './Paragraph';
-import List from './List';
+import './index.css';
 
 function App() {
+
+  let curDate = new Date(2020,7,1,2);
+curDate = curDate.getHours();
+let greeting = "";
+
+const cssStyle = { }
+
+if(curDate >= 1 && curDate < 12){
+  greeting="Good Morning";
+  cssStyle.color='green';
+}else if (curDate >=12 && curDate < 19){
+  greeting ="Good Afternoon";
+  cssStyle.color='orange';
+}else{
+  greeting ="Good Night";
+  cssStyle.color='dark';
+}
   return (
     <>
-      <Heading />
-      <Paragraph />
-      <List />
-      <Paragraph/>
+      <div>
+      <h1>
+      Hello Sir, <span style={cssStyle} >{greeting} </span>
+      </h1>
+      </div>
     </>
   );
 
