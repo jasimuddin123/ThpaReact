e7ceac7341f982a0d08c04d5e5b34b434c33c2fd
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-15 08:28:03 -0700

    #24 Completing React JS Netflix App #3 - Array Map & Fat Arrow function in React Js

--- src/Sdata.jsx
+++ src/Sdata.jsx
@@ -30,6 +30,12 @@ const Sdata = [
         imgsrc: "https://picsum.photos/id/4/200/300",
         title: "A Netflix Original Series",
         link: "https://picsum.photos/images",
-    }
+    },
+    {
+        sname: "Dark",
+        imgsrc: "https://picsum.photos/id/1/200/300",
+        title: "A Netflix Original Series",
+        link: "https://picsum.photos/images",
+    },
 ];
 export default Sdata;
--- src/index.js
+++ src/index.js
@@ -4,43 +4,34 @@ import Cards from './Cards';
 import Sdata from './Sdata';
 
 
+// function ncard(val){
+//   console.log(val);
+//   return(
+//     <Cards
 
+//     imgsrc={val.imgsrc}
+//     title={val.title}
+//     sname={val.sname}
+//     link={val.link}
+// />
+//   )
+// }
 ReactDOM.render(
 <>
 <h2 className="heading_style">List of Five favorite Movie </h2>
-<Cards
 
-    imgsrc={Sdata[0].imgsrc}
-    title={Sdata[0].title}
-    sname={Sdata[0].sname}
-    link={Sdata[0].link}
+  {Sdata.map((val,index) =>{
+    return(
+    <Cards
+
+    imgsrc={val.imgsrc}
+    title={val.title}
+    sname={val.sname}
+    link={val.link}
 />
+  )
 
-<Cards
-
-    imgsrc={Sdata[1].imgsrc}
-    title={Sdata[1].title}
-    sname={Sdata[1].sname}
-    link={Sdata[1].link}
-/>
-
-
-<Cards
-
-    imgsrc={Sdata[2].imgsrc}
-    title={Sdata[2].title}
-    sname={Sdata[2].sname}
-    link={Sdata[2].link}
-/>
-
-
-<Cards
-
-    imgsrc={Sdata[3].imgsrc}
-    title={Sdata[3].title}
-    sname={Sdata[3].sname}
-    link={Sdata[3].link}
-/>
+  })}
 
 </>,
  document.getElementById('root')
