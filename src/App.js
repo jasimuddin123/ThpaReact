6a8c14ccf412c8006135281d20785885f9aee31a
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-20 08:24:41 -0700

    #30: Hooks in React JS in Hindi | useState in Hook in React JS in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Cards from './Cards';
 import Sdata from './Sdata';
 import Netflix from './Netflix';
@@ -18,14 +18,32 @@ const favSeries = 'amazon';
 
 // }
 
-const App = () => (
+
+
+const App = () => {
+
+  const state = useState();
+// console.log(state);
+// let count = 1;
+
+const [count,setCount]  = useState(0);
+
+const IncNum = ()=>{
+  setCount(count+1);
+  // console.log("clicked " + count++);
+};
+  return(
   <>
     <h2 className="heading_style">List of Five favorite Movie </h2>
     {/* <FavS /> */}
     {(favSeries === 'netfilix') ? <Netflix/> : <Amazon/>}
 
-  </>
+    <h1>{count}</h1>
+    <button onClick={IncNum}> Click</button>
 
-)
+  </>
+  )
+
+}
 
 export default App;
