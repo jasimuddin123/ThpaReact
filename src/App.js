8647001b5c02ca10131126e975590355112d27fd
jasimuddin123 <jasimuddindxn@gmail.com>
2020-07-23 08:13:26 -0700

    #33: Handling Events in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,78 +1,30 @@
-import React,{useState} from 'react';
-
-
-import Netflix from './Netflix';
-import Amazon from './Amazon';
+import React, {useState} from 'react';
 import './App.css';
 
-const favSeries = 'amazon';
 
-// const FavS = () => {
-//   if (favSeries === 'netfilix') {
-//      return <Netflix></Netflix>
+const App =()=>{
+  const purple = "#8e44ad"
+  const [bg, setBg] = useState(purple);
+  const [name, setName] = useState("click Me");
 
-//   } else {
+  const bgChange = ()=>{
+    let newBg = '#34495e';
+    setBg(newBg);
+    setName("Ouch");
+  };
 
-//     return (<Amazon></Amazon> );
-
-//   }
-
-// }
-
-
-
-const App = () => {
-
-  const state = useState();
-// console.log(state);
-// let count = 1;
-
-const [count,setCount]  = useState(0);
-
-const IncNum = ()=>{
-  setCount(count+1);
-  // console.log("clicked " + count++);
-};
-
-let newTime = new Date().toLocaleTimeString();
-
-const [time,setCtime] = useState(newTime);
-
-  const updateTime = ()=>{
-    let newCtime = new Date().toLocaleTimeString();
-
-    setCtime(newCtime);
+  const bgBack = ()=>{
+    setBg(purple);
+    setName("Ayyo!!");
 
   }
-
-  let justTime = new Date().toLocaleTimeString();
-  const [dTime, dSetTime] = useState(justTime);
-
-  const dynamicTime = ()=>{
-   justTime = new Date().toLocaleTimeString();
-    dSetTime(justTime);
-  };
-    setInterval(dynamicTime,1000);
-
   return(
-  <>
-    <h1> Dynamic Time = {dTime}</h1>
-    
-
-
-
-    <h2> {time} </h2>
-    <button onClick={updateTime}> Get Time </button>
-    <h2 className="heading_style">List of Five favorite Movie </h2>
-    {/* <FavS /> */}
-    {(favSeries === 'netfilix') ? <Netflix/> : <Amazon/>}
-
-    <h1>{count}</h1>
-    <button onClick={IncNum}> Click</button>
-
-  </>
+    <>
+    <div style = {{backgroundColor:bg}}>
+    <button onMouseEnter={bgChange} onMouseLeave={bgBack}> {name} </button>
+    </div>
+    </>
   )
-
 }
 
 export default App;
